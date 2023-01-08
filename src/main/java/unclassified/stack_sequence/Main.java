fb9d0a1417e0c822d3a046ea68afa4cbe779e72d
sunba <sunba2318@gmail.com>
2023-01-08 19:01:05 +0900

    solved 1874 스택 수열

---
+++ src/main/java/unclassified/stack_sequence/Main.java
@@ -0,0 +1,35 @@
+package unclassified.stack_sequence;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int count = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < T; i++) {
+            int nextInt = Integer.parseInt(br.readLine());
+            if (count < nextInt) {
+                for (int j = count; j < nextInt; j++) {
+                    stack.push(++count);
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                if (stack.peek() == nextInt) {
+                    stack.pop();
+                    sb.append("-\n");
+                } else {
+                    sb.replace(0, sb.length(), "NO");
+                    break;
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
