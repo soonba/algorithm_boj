a31a5d19147de862506f8993801b0d6f80470f9b
sunba <sunba2318@gmail.com>
2023-01-08 15:49:56 +0900

    solved 1085 직사각형에서 탈출

---
+++ src/main/java/math/rectangle/Main.java
@@ -0,0 +1,23 @@
+package math.rectangle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int min = Math.min(x, y);
+        int min1 = Math.min(w - x, h - y);
+        System.out.println(Math.min(min,min1));
+
+    }
+}
