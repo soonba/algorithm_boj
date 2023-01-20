85146f9231b4e53e69be491cc6e8bfa1833f533d
sunba <sunba2318@gmail.com>
2023-01-20 12:18:48 +0900

    chore 2164 주석

--- src/main/java/data_structure/card2/Main.java
+++ src/main/java/data_structure/card2/Main.java
@@ -5,6 +5,20 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/*
+ * BAEKJOON ONLINE JUDGE
+ * https://www.acmicpc.net
+ * Problem Number: 2164
+ * Level: Silver V
+ * Algorithm: Queue
+*/
+
+/*
+입력값을 Queue에 담는다.
+버리고(poll()) 제일 뒤에 추가 (poll() + add()) 하여 사이즈가 1일 때 까지 진행한다.
+마지막 poll()을 출력한다.
+*/
+
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
