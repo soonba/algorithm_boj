9d411c351998d8533f212b342ab2738e2e376029
sunba <sunba2318@gmail.com>
2023-02-11 19:27:32 +0900

    solved 1229 육각수

--- src/main/java/brute_force/a_and_b/Main.java
+++ src/main/java/brute_force/a_and_b/Main.java
@@ -3,6 +3,7 @@ package brute_force.a_and_b;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+//23.2.11 unsolved!
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
