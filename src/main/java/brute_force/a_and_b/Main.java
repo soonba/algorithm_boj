343e843abac28c309fe4ce7bb9b50c9705117945
sunba <sunba2318@gmail.com>
2023-02-26 20:19:18 +0900

    solved 12919 A와 B 2

--- src/main/java/brute_force/a_and_b/Main.java
+++ src/main/java/brute_force/a_and_b/Main.java
@@ -2,38 +2,50 @@ package brute_force.a_and_b;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
-//23.2.11 unsolved!
+
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
         String T = br.readLine();
 
-        while(T.length() >= S.length() +1) {
-            String firstStr = T.substring(0, 1);
-            String lastStr = T.substring(T.length()-1);
-            if(lastStr.equals("A")) {
-                T = T.substring(0,T.length()-1);
+        boolean convertable = false;
+        Queue<String> queue = new LinkedList<>();
+        queue.add(T);
+        while (!queue.isEmpty()) {
+            String poll = queue.poll();
+            if(poll.length() < S.length()) {
+                continue;
             }
-
-            if(S.length() == T.length()) {
-                int result = S.equals(T) ? 1 : 0;
-                System.out.println(result);
+            if(poll.equals(S)) {
+                convertable = true;
                 break;
             }
+            char first = poll.charAt(0);
+            char last = poll.charAt(poll.length()-1);
 
-            if(firstStr.equals("B")) {
-                T = T.substring(1);
-                StringBuilder newT = new StringBuilder();
-                for (int i = T.length() -1 ; i >= 0; i--) {
-                    newT.append(T.charAt(i));
-                }
-                T = newT.toString();
+            if (first == 'A' && last == 'A') {
+                queue.add(poll.substring(0,poll.length()-1));
+            } else if (first == 'B' && last == 'B' ) {
+                String minusFirst = poll.substring(1);
+                StringBuilder sb = new StringBuilder(minusFirst);
+                queue.add(sb.reverse().toString());
+            } else if (first == 'B' && last == 'A') {
+                queue.add(poll.substring(0,poll.length()-1));
+                String minusFirst = poll.substring(1);
+                StringBuilder sb = new StringBuilder(minusFirst);
+                queue.add(sb.reverse().toString());
             }
+
         }
 
-        int result = S.equals(T) ? 1 : 0;
-        System.out.println(result);
+        if(convertable) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
     }
 }
