b9761f7da85cdcfe087ec6f90e80b7bc7590b86a
sunba <sunba2318@gmail.com>
2023-03-04 15:16:03 +0900

    solved 1012 유기농 배추 - refactoring

--- src/main/java/data_structure/organic_cabbage/Main.java
+++ src/main/java/data_structure/organic_cabbage/Main.java
@@ -1,28 +1,24 @@
 package data_structure.organic_cabbage;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int[] dx = {1,-1,0,0};
-        int[] dy = {0,0,1,-1};
+        int T = read();
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
             int bugCnt = 0;
-            String[] s = br.readLine().split(" ");
-            int R = Integer.parseInt(s[0]);
-            int C = Integer.parseInt(s[1]);
-            int cabbageCnt = Integer.parseInt(s[2]);
+            int R = read();
+            int C = read();
+            int cabbageCnt = read();
             boolean[][] matrix = new boolean[R][C];
             for (int j = 0; j < cabbageCnt; j++) {
-                String[] s1 = br.readLine().split(" ");
-                int i0 = Integer.parseInt(s1[0]);
-                int i1 = Integer.parseInt(s1[1]);
+                int i0 = read();
+                int i1 = read();
                 matrix[i0][i1] = true;
             }
 
@@ -39,9 +35,9 @@ public class Main {
                             for (int l = 0; l < 4; l++) {
                                 int tempR = nowR + dx[l];
                                 int tempC = nowC + dy[l];
-                                if(tempR < 0 || tempC < 0 || tempR >= R || tempC >= C) continue;
-                                if(matrix[tempR][tempC]) {
-                                    queue.add(new int[]{tempR,tempC});
+                                if (tempR < 0 || tempC < 0 || tempR >= R || tempC >= C) continue;
+                                if (matrix[tempR][tempC]) {
+                                    queue.add(new int[]{tempR, tempC});
                                     matrix[tempR][tempC] = false;
                                 }
                             }
@@ -50,7 +46,14 @@ public class Main {
                     }
                 }
             }
-            System.out.println(bugCnt);
+            sb.append(bugCnt).append("\n");
         }
+        System.out.println(sb);
+    }
+
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
 }
