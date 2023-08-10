19e998dd08d8561437bcdf484a539828303d9cd4
sunba <sunba2318@gmail.com>
2023-08-11 00:52:25 +0900

    test1

---
+++ src/main/java/programmers/test/Main.java
@@ -0,0 +1,13 @@
+package programmers.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        String ab = solution.solution("aAzZ", 25);
+        System.out.println(ab);
+
+    }
+}
