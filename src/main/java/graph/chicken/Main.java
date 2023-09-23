214383ed62cd50f0e2fc6f316523b7dad62d84d7
sunba <sunba2318@gmail.com>
2023-09-23 14:11:41 +0900

    solved 15686 치킨 (리팩)

--- src/main/java/graph/chicken/Main.java
+++ src/main/java/graph/chicken/Main.java
@@ -7,8 +7,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Position {
-    private final int row;
-    private final int col;
+    private final int row, col;
 
     public Position(int row, int col) {
         this.row = row;
@@ -28,39 +27,49 @@ class Position {
     }
 }
 
-class Chicken {
-    private final Position position;
-
-    public Chicken(Position position) {
-        this.position = position;
-    }
-
-    public Position getPosition() {
-        return position;
-    }
-}
-
-class House {
-    private final Position position;
-
-    public House(Position position) {
-        this.position = position;
-    }
-
-    public Position getPosition() {
-        return position;
-    }
-}
-
-//치킨집, 집을 리스트에 담는다. n^2
-//모든 집을 순회하며 가까운 치킨집 순의 리스트를 담는다.
-//치킨집 조합 배열을 순회하고 그 안에서 집을 순회하며 각 집별 가장 가까운 치킨집 include 의 거리 값을 계산한다.
-//거리값의 합의 최소를 출력한다.
-
 public class Main {
     static final List<List<Integer>> combinationList = new ArrayList<>();
-    static List<Chicken> chickenList = new ArrayList<>();
-    static List<House> houseList = new ArrayList<>();
+    static List<Position> chickenList = new ArrayList<>();
+    static List<Position> houseList = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        int N = read(); // n x n
+        int M = read(); // 최대 개수
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int now = read();
+                if (now == 1) {
+                    houseList.add(new Position(i, j));
+                }
+
+                if (now == 2) {
+                    chickenList.add(new Position(i, j));
+                }
+            }
+        }
+
+        //치킨집의 크기만큼의 int 리스트 생성
+        List<Integer> combinationTarget = IntStream.range(0, chickenList.size())
+                .boxed()
+                .collect(Collectors.toList());
+        combination(combinationTarget, new boolean[combinationTarget.size()], 0, combinationTarget.size(), M);
+
+        //각 조합을 돌며 최소값 갱신
+        int minScore = Integer.MAX_VALUE;
+        for (List<Integer> aCombination : combinationList) {
+            int tempCount = 0;
+            for (Position house : houseList) {
+                tempCount += aCombination.stream().map(m -> {
+                    Position chicken = chickenList.get(m);
+                    return house.calculate(chicken);
+                }).min(Comparator.comparingInt(a -> a)).get();
+            }
+            minScore = Math.min(minScore, tempCount);
+        }
+
+        System.out.println(minScore);
+    }
 
     static void addCombination(List<Integer> arr, boolean[] visited, int n) {
         ArrayList<Integer> aCombination = new ArrayList<>();
@@ -83,45 +92,6 @@ public class Main {
             combination(arr, visited, i + 1, n, r - 1);
             visited[i] = false;
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        int N = read(); // n x n
-        int M = read(); // 최대 개수
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                int now = read();
-                if (now == 1) {
-                    houseList.add(new House(new Position(i, j)));
-                }
-
-                if (now == 2) {
-                    chickenList.add(new Chicken(new Position(i, j)));
-                }
-            }
-        }
-
-        //치킨집의 크기만큼의 int 리스트 생성
-        List<Integer> combinationTarget = IntStream.range(0, chickenList.size())
-                .boxed()
-                .collect(Collectors.toList());
-        combination(combinationTarget, new boolean[combinationTarget.size()], 0, combinationTarget.size(), M);
-
-        //각 조합을 돌며 최소값 갱신
-        int minScore = Integer.MAX_VALUE;
-        for (List<Integer> aCombination : combinationList) {
-            int tempCount = 0;
-            for (House house : houseList) {
-                tempCount += aCombination.stream().map(m -> {
-                    Chicken chicken = chickenList.get(m);
-                    return house.getPosition().calculate(chicken.getPosition());
-                }).min(Comparator.comparingInt(a -> a)).get();
-            }
-            minScore = Math.min(minScore, tempCount);
-        }
-
-        System.out.println(minScore);
     }
 
     private static int read() throws Exception {
