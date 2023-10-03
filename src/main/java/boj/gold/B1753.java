16219fc78b778ee8199259a87fd7c1489f4d9154
sunba <sunba2318@gmail.com>
2023-10-03 12:45:44 +0900

    solved 1753 최단경로 (설명추)

--- src/main/java/boj/gold/B1753.java
+++ src/main/java/boj/gold/B1753.java
@@ -5,51 +5,67 @@ import java.util.*;
 //다익스트라 우선순위 큐
 public class B1753 {
     public static void main(String[] args) throws Exception {
-        int node = read();
-        int line = read();
-        int begin = read()-1;
+        //vertex
+        int v = read();
+        //edge
+        int e = read();
+        int beginIndex = read() - 1;
 
-        int[] costs = new int[node];
-        Arrays.fill(costs,Integer.MAX_VALUE);
+        //최소비용배열 - 노드의 개수만큼
+        int[] costs = new int[v];
+        //모든 비용 무한대로 설정
+        Arrays.fill(costs, Integer.MAX_VALUE);
 
+        //edge 정보 담은 그래프
         List<List<Node>> graph = new ArrayList<>();
-        for (int i = 0; i < node; i++) {
+        // 그래프 리스트 초기화
+        for (int i = 0; i < v; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < line; i++) {
+        for (int i = 0; i < e; i++) {
             int start = read()-1;
             int end = read()-1;
             int cost = read();
+            //start 그래프에 목적지 index가 end이고 비용이 cost인 Node 추가
             graph.get(start).add(new Node(end,cost));
         }
 
+        //queue 를 생성하고 queue에 시작지점, 비용 0의 노드 추가
         Queue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(Node::getDistance));
-        pq.add(new Node(begin,0));
-        costs[begin] = 0;
+        pq.add(new Node(beginIndex,0));
+        //시작지점의 비용은 0
+        costs[beginIndex] = 0;
 
-        while(!pq.isEmpty()) {
-            Node n = pq.poll();
-            int i = n.getIndex();
-            int d = n.getDistance();
-            if(costs[i] < d) continue;
+        while (!pq.isEmpty()) {
+            Node poll = pq.poll();
+            //현재 노드의 인덱스
+            int now = poll.getIndex();
 
-            List<Node> nowGraph = graph.get(i);
-            for (int j = 0; j < nowGraph.size(); j++) {
-                int cost = costs[i] + nowGraph.get(j).getDistance();
-                if(cost < costs[nowGraph.get(j).getIndex()]) {
-                    costs[nowGraph.get(j).getIndex()] = cost;
-                    pq.add(new Node(nowGraph.get(j).getIndex(),cost));
+            //시작지점에서 현재 노드까지의 단순 주어진 비용(최신화 고려 안됨)
+            int distance = poll.getDistance();
+
+            //현재 노드의 인덱스 최소 비용이 단순비용보다 이미 작다면 통과
+            if(costs[now] < distance) continue;
+
+            //아니라면 해당 노드가 가진 간선들에 대해 순회
+            //해당 노드가 가진 노드들
+            List<Node> nodes = graph.get(now);
+            for (int i = 0; i < nodes.size(); i++) {
+                //기존 최소비용에 해당 노드의 비용을 더한 값이 새로운 비용
+                int newCost = costs[now] + nodes.get(i).getDistance();
+                //만약 새로운 값이 해당 노드의 인덱스까지의 최소비용보다 작다면 갱신하고, 시작지점에서 해당 노드까지의 비용 노드 추가
+                int targetIndex = nodes.get(i).getIndex();
+                if(newCost < costs[targetIndex]) {
+                    costs[targetIndex] = newCost;
+                    pq.add(new Node(targetIndex,newCost));
                 }
             }
         }
 
-        for (int i = 0; i < node; i++) {
-            if(costs[i] == Integer.MAX_VALUE) {
-                System.out.println("INF");
-            } else {
-                System.out.println(costs[i]);
-            }
+        for (int cost : costs) {
+            String out = cost == Integer.MAX_VALUE ? "INF" : String.valueOf(cost);
+            System.out.println(out);
         }
     }
 
@@ -64,16 +80,16 @@ class Node {
     private final int index;
     private final int distance;
 
+    public Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
     public int getIndex() {
         return index;
     }
 
     public int getDistance() {
         return distance;
-    }
-
-    public Node(int index, int distance) {
-        this.index = index;
-        this.distance = distance;
     }
 }
