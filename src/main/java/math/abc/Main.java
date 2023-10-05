07290d166ad69a5083ef6f1f29d3bfbe15bebd88
sunba <sunba2318@gmail.com>
2023-10-05 23:45:53 +0900

    solved ? - ABC 곱셈

---
+++ src/main/java/math/abc/Main.java
@@ -0,0 +1,35 @@
+package math.abc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[] split = s.split(" ");
+
+        long A = Integer.parseInt(split[0]);
+        long B = Integer.parseInt(split[1]);
+        long C = Integer.parseInt(split[2]);
+//        A^B % C. (A * A') % C = (A%C * A'%C) % C
+
+        long result = calc(A,B,C);
+
+        System.out.println(result);
+
+    }
+
+    private static long calc(long A, long B, long C) {
+        if(B==1) {
+            return A % C;
+        }
+
+        if(B%2 == 0) {
+            long temp = calc(A, B / 2, C);
+            return (temp * temp) % C;
+        } else {
+            return ((calc(A,B-1,C)) * (A % C)) % C;
+        }
+    }
+}
