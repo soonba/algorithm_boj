37fee793bf45169979a649a0e2f2cd976470b625
sunba <sunba2318@gmail.com>
2023-10-11 20:20:43 +0900

    solved 1967 트리의 지름(DFS)

--- src/main/java/boj/gold/B1967/Main.java
+++ src/main/java/boj/gold/B1967/Main.java
@@ -1,92 +1,73 @@
 package boj.gold.B1967;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    static int[][] initGraph;
-    static int N;
-    static final int MAX_DISTANCE = 1000 * 100;
+
+    static List<Node>[] graph;
+    static int max = 0;
+    static int maxIdx = 0;
+    static boolean[] visits;
     public static void main(String[] args) throws Exception {
-        N = read();
-        initGraph = new int[N][N];
+        int N = read();
+        graph  = new ArrayList[N];
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(i==j) continue;
-                initGraph[i][j] = MAX_DISTANCE;
-            }
+            graph[i] = new ArrayList<>();
         }
         for (int i = 0; i < N - 1; i++) {
-            int start = read()-1;
-            int end = read()-1;
+            int start = read() - 1;
+            int end = read() - 1;
             int distance = read();
-            initGraph[start][end] = distance;
-            initGraph[end][start] = distance;
+            graph[start].add(new Node(end,distance));
+            graph[end].add(new Node(start, distance));
         }
 
-        int[][] zeroToMax = dijkstra(0);
-        int maxIndex = 0;
-        int max = 0;
-        int[] zeroToMax1 = zeroToMax[0];
-        for (int i = 0; i < N; i++) {
-            int now = zeroToMax1[i];
-            if(now == MAX_DISTANCE) continue;
-            max = Math.max(now,max);
-            if(max == now) {
-                maxIndex = i;
-            }
-        }
-
-        int[][] dijkstra = dijkstra(maxIndex);
-        int max2 = 0;
-        int[] zeroToMax2 = dijkstra[maxIndex];
-        for (int i = 0; i < N; i++) {
-            int now2 = zeroToMax2[i];
-            if(now2 == MAX_DISTANCE) continue;
-            max2 = Math.max(now2,max2);
-        }
-        System.out.println(max2);
-
+        visits = new boolean[N];
+        dfs(0, 0);
+        visits = new boolean[N];
+        dfs(maxIdx, 0);
+        System.out.println(max);
     }
 
-    private static int[][] dijkstra(int startIndex) {
-        int[][] graph = initGraph;
-        boolean[] visits = new boolean[N];
-        visits[startIndex] = true;
-        for (int i = 0; i < N; i++) {
-            int minIndex = findMinIndex(startIndex, graph, visits);
-            if(minIndex == N) continue;
-            int[] targetGraph = graph[minIndex];
-            for (int j = 0; j < targetGraph.length; j++) {
-                if(targetGraph[j] ==0) continue;
-                graph[startIndex][j] = Math.min(graph[startIndex][minIndex]+targetGraph[j], graph[startIndex][j] );
+    private static void dfs(int now, int temp) {
+        List<Node> nodes = graph[now];
+        visits[now] = true;
+        for (int i = 0; i < nodes.size(); i++) {
+            Node node = nodes.get(i);
+            if(visits[node.getIndex()]) {
+                max = Math.max(max, temp);
+                if(temp == max) {
+                    maxIdx = now;
+                }
+                continue;
             }
-            visits[minIndex] = true;
+            dfs(node.getIndex(),temp + node.getDistance());
         }
-
-        return graph;
-
-    }
-
-    private static int findMinIndex(int start, int[][] graph, boolean[] visits) {
-        int[] aNodeGraph = graph[start];
-        int min = MAX_DISTANCE;
-        int minIndex = N;
-        for (int i = 0; i < aNodeGraph.length; i++) {
-            //방문했다면 스킵
-            if(visits[i]) continue;
-            int now = aNodeGraph[i];
-            //최대값이면 스킵
-            if(now == MAX_DISTANCE) continue;
-            if (Math.min(min, now) != min) {
-                min = now;
-                minIndex = i;
-            }
-        }
-        return minIndex;
     }
 
     private static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
         return n;
+    }
+}
+
+class Node {
+    private final int index;
+    private final int distance;
+
+    public Node(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
