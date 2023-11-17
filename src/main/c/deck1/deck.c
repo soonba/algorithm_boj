147b943ad0f0ca9080f064d74665535fd51f75b3
sunba <sunba2318@gmail.com>
2023-11-17 17:30:30 +0900

    c

---
+++ src/main/c/deck1/deck.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 0; i < T; i++) {
+        char str01[15];
+        // scanf("%s", &str01);
+        // printf("%s",str01);
+    }
+    
+}
