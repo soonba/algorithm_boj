1631c4cf1ebcb5d8e3eb616aacf6864527c21572
sunba <sunba2318@gmail.com>
2023-11-19 15:33:39 +0900

    solved 123

---
+++ src/main/java/boj/bronze/B123/Main.java
@@ -0,0 +1,14 @@
+package boj.bronze.B123;
+
+//\    /\
+//  )  ( ')
+//(  /  )
+//  \(__)|
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
