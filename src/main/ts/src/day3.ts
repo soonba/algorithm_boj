632048b7c60808dc35e680d99d65d4d22f7fcd6d
sunba <sunba2318@gmail.com>
2025-05-25 16:36:50 +0900

    solved

--- src/main/ts/src/day3.ts
+++ src/main/ts/src/day3.ts
@@ -1,36 +1,34 @@
 import { promises as fs } from 'fs';
 
 const TREE = '#';
+const SLOPES = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
 
 const loadInputs = async (path: string): Promise<string[]> => {
   const input = (await fs.readFile(path, 'utf-8')).trimEnd();
   return input.split('\n');
 };
 
-const calculateHitTrees = (inputs: string[], xDiff: number, yDiff: number) => {
-  const maxIdx = inputs[0].length;
+const calculateHitTrees = (inputs: string[], dx: number, dy: number): number => {
+  const len = inputs[0].length;
   let x = 0;
-  let count = 0;
-  for (let y = 0; y < inputs.length; y += yDiff) {
-    if (inputs[y][x % maxIdx] === TREE) count++;
-    x += xDiff;
+  let hit = 0;
+  for (let y = 0; y < inputs.length; y += dy) {
+    if (inputs[y][x % len] === TREE) hit++;
+    x += dx;
   }
-  return count;
+  return hit;
 };
 
 export const day3 = async () => {
   const inputs = await loadInputs(`./input/day3.txt`);
-  const slopes = [
-    [1, 1],
-    [3, 1],
-    [5, 1],
-    [7, 1],
-    [1, 2],
-  ];
-  const total = slopes
-    .map(([x, y]) => calculateHitTrees(inputs, x, y))
-    .reduce((acc, curr) => acc * curr, 1);
+  const product = (acc: number, curr: number) => acc * curr;
+  const total = SLOPES.map(([dx, dy]) => calculateHitTrees(inputs, dx, dy)).reduce(product, 1);
   console.log(total);
 };
-
 day3();
